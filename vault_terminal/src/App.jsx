fda63a542650af26f0830c80f70240402cc42aae
Pattarapol Tantechasa <froyplus@hotmail.co.th>
2024-12-01 16:25:33 +1100

    Project Setup Done

---
+++ vault_terminal/src/App.jsx
@@ -0,0 +1,10 @@
+import GameBoard from "./components/GameBoard";
+function App() {
+  return (
+    <main className=" w-screen h-screen flex justify-center items-center">
+      <GameBoard />
+    </main>
+  );
+}
+
+export default App;
